67eca60f6dbe27e0f089ec8bb72f1a572b3b40bf
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2024-03-20 17:04:56 +0530

    URL

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,12 +1,16 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from '../Pages/Home'
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "../Pages/Home";
 
 const Router = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<Navigate to="/calcolo-stipendio-lordo-netto" replace />}
+      />
+      <Route path="/calcolo-stipendio-lordo-netto" element={<Home />} />
+    </Routes>
+  );
+};
 
-export default Router
+export default Router;
